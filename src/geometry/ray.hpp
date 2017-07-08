6083d823ba1cfaec79039952dd7090c616264bb4
charly <charly@jean-michel.com>
2017-07-08 20:32:38 +0200

    [ray] completed struct

--- src/geometry/ray.hpp
+++ src/geometry/ray.hpp
@@ -1,11 +1,20 @@
 #pragma once
 
-#include <vector>
+#include <geometry/point.hpp>
 
 template<typename Type, int Dimension>
 struct Ray
 {
+    Point<Type,Dimension> origin;
+    //I choosed to set dimension type as a Point...
+    //fundamentaly this is not a point but anyway
+    Point<Type,Dimension> direction;
 
+    Ray(const Point<Type,Dimension>& o = Point<Type,Dimension>()
+      , const Point<Type,Dimension>& d = Point<Type,Dimension>()
+    )
+    : origin(o), direction(d)
+    {}
 };
 
 using Ray2D = Ray<double,2>;
@@ -13,3 +22,12 @@ using Ray3D = Ray<double,3>;
 
 using Rays2D = std::vector<Ray2D>;
 using Rays3D = std::vector<Ray3D>;
+
+template<typename Type, int Dimension>
+std::ostream& operator<<(std::ostream& o, const Ray<Type,Dimension>& r)
+{
+    o << "origin: " << r.origin << "\n";
+    o << "direction: " << r.direction;
+
+    return o;
+}
