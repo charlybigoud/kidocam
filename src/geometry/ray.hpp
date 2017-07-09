4a942b22b1b7f68b7d440e6d2ebb9dcd0635d19e
charly <charly@jean-michel.com>
2017-07-09 12:55:19 +0200

    [ray] full eigen

--- src/geometry/ray.hpp
+++ src/geometry/ray.hpp
@@ -1,17 +1,48 @@
 #pragma once
 
-#include <geometry/point.hpp>
+#include <eigen3/Eigen/Dense>
+
+#include <utils/types.hpp>
+
+// template<typename Type, int Dimension>
+// struct Ray
+// {
+//     Point<Type,Dimension> origin;
+//     //I choosed to set dimension type as a Point...
+//     //fundamentaly this is not a point but anyway
+//     Point<Type,Dimension> direction;
+
+//     Ray(const Point<Type,Dimension>& o = Point<Type,Dimension>()
+//       , const Point<Type,Dimension>& d = Point<Type,Dimension>()
+//     )
+//     : origin(o), direction(d)
+//     {}
+// };
+
+// using Ray2D = Ray<double,2>;
+// using Ray3D = Ray<double,3>;
+
+// using Rays2D = std::vector<Ray2D>;
+// using Rays3D = std::vector<Ray3D>;
+
+// template<typename Type, int Dimension>
+// std::ostream& operator<<(std::ostream& o, const Ray<Type,Dimension>& r)
+// {
+//     o << "origin: " << r.origin << "\n";
+//     o << "direction: " << r.direction;
+
+//     return o;
+// }
+
 
 template<typename Type, int Dimension>
 struct Ray
 {
-    Point<Type,Dimension> origin;
-    //I choosed to set dimension type as a Point...
-    //fundamentaly this is not a point but anyway
-    Point<Type,Dimension> direction;
+    Eigen::Matrix<Type,Dimension,1> origin;
+    Eigen::Matrix<Type,Dimension,1> direction;
 
-    Ray(const Point<Type,Dimension>& o = Point<Type,Dimension>()
-      , const Point<Type,Dimension>& d = Point<Type,Dimension>()
+    Ray(const Eigen::Matrix<Type,Dimension,1>& o = Eigen::Matrix<Type,Dimension,1>::Zero()
+      , const Eigen::Matrix<Type,Dimension,1>& d = Eigen::Matrix<Type,Dimension,1>::Zero()
     )
     : origin(o), direction(d)
     {}
@@ -20,8 +51,8 @@ struct Ray
 using Ray2D = Ray<double,2>;
 using Ray3D = Ray<double,3>;
 
-using Rays2D = std::vector<Ray2D>;
-using Rays3D = std::vector<Ray3D>;
+using Rays2D = AlignedVector<Ray2D>;
+using Rays3D = AlignedVector<Ray3D>;
 
 template<typename Type, int Dimension>
 std::ostream& operator<<(std::ostream& o, const Ray<Type,Dimension>& r)
