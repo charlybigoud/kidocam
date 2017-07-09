e0ecf4f66a6f479b6b9455598682949a36c032a9
charly <charly@jean-michel.com>
2017-07-09 12:54:47 +0200

    [point] full eigen

--- src/geometry/point.hpp
+++ src/geometry/point.hpp
@@ -5,52 +5,58 @@
 
 #include <eigen3/Eigen/Dense>
 
-template<typename Type, int Dimension>
-struct Point
-{
-    // Type data[Dimension];
-    Eigen::Matrix<double,Dimension,1> data;
+#include <utils/types.hpp>
 
-    template<typename ... X>
-    Point(X ... t)
-    : data{ Type(t)... }
-    {}
+// template<typename Type, int Dimension>
+// struct Point
+// {
+//     Type data[Dimension];
 
-    ~Point(){}
+//     template<typename ... X>
+//     Point(X ... t)
+//     : data{ Type(t)... }
+//     {}
 
-    template<int I>
-    Type& at()
-    {
-        static_assert(Dimension > I,"Error: Point::at() out of range.");
-        return data[I];
-    }
+//     ~Point(){}
 
-    template<int I>
-    const Type& at() const
-    {
-        static_assert(Dimension > I,"Error: Point::at() out of range.");
-        return data[I];
-    }
+//     template<int I>
+//     Type& at()
+//     {
+//         static_assert(Dimension > I,"Error: Point::at() out of range.");
+//         return data[I];
+//     }
 
-    Type& x() { return at<0>(); }
-    Type& y() { return at<1>(); }
-    Type& z() { return at<2>(); }
+//     template<int I>
+//     const Type& at() const
+//     {
+//         static_assert(Dimension > I,"Error: Point::at() out of range.");
+//         return data[I];
+//     }
 
-    const Type& x() const { return at<0>(); }
-    const Type& y() const { return at<1>(); }
-    const Type& z() const { return at<2>(); }
+//     Type& x() { return at<0>(); }
+//     Type& y() { return at<1>(); }
+//     Type& z() { return at<2>(); }
 
-    Type norm() const
-    {
-        return spec_norm(*this);
-    }
-};
+//     const Type& x() const { return at<0>(); }
+//     const Type& y() const { return at<1>(); }
+//     const Type& z() const { return at<2>(); }
 
-using P2D = Point<double, 2>;
-using P3D = Point<double, 3>;
+//     Type norm() const
+//     {
+//         return spec_norm(*this);
+//     }
+// };
 
-using P2DS = std::vector<P2D>;
-using P3DS = std::vector<P3D>;
+// using P2D = Point<double, 2>;
+// using P3D = Point<double, 3>;
+
+
+//eigen dependent type
+using P2D = Eigen::Matrix<double, 2, 1>;
+using P3D = Eigen::Matrix<double, 3, 1>;
+
+using P2DS = AlignedVector<P2D>;
+using P3DS = AlignedVector<P3D>;
 
 std::ostream& operator<<(std::ostream& o, const P2D& p);
 std::ostream& operator<<(std::ostream& o, const P3D& p);
