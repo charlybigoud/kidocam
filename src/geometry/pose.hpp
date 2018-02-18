744ca06624e0f99f5e460578355b08d4ed0d0145
charly <charly@jean-michel.com>
2018-02-18 18:44:40 +0100

    [pose] added comments

--- src/geometry/pose.hpp
+++ src/geometry/pose.hpp
@@ -10,12 +10,17 @@ struct Pose
     Eigen::Matrix<double, Dimension, Dimension> rotation;
     Eigen::Matrix<double, Dimension, 1> translation;
 
-    Pose(const Eigen::Matrix<double, Dimension, Dimension>& r = Eigen::Matrix<double, Dimension, Dimension>::Identity()
-       , const Eigen::Matrix<double, Dimension, 1>& t = Eigen::Matrix<double, Dimension,1>::Zero()
-    )
+    /*
+     * Pose constructor
+    **/
+    Pose(const Eigen::Matrix<double, Dimension, Dimension>& r = Eigen::Matrix<double, Dimension, Dimension>::Identity(),
+         const Eigen::Matrix<double, Dimension, 1>& t = Eigen::Matrix<double, Dimension,1>::Zero())
     : rotation(r), translation(t)
     {}
 
+    /*
+     * Pose destructor
+    **/
     ~Pose(){};
 };
 
@@ -25,6 +30,9 @@ using Pose3D = Pose<3>;
 using Poses2D = AlignedVector<Pose2D>;
 using Poses3D = AlignedVector<Pose3D>;
 
+/*
+ * the custom operator<< for Pose
+**/
 template<int Dimension>
 std::ostream& operator<<(std::ostream& os, const Pose<Dimension>& p)
 {
